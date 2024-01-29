fe77eafd4a6f0a4086e60290c2f89056c5275a52
Kunal <kunalukey32@gmail.com>
2024-01-29 19:35:57 +0530

    added args in puppeteer

--- tests/helpers/page.js
+++ tests/helpers/page.js
@@ -7,6 +7,7 @@ class CustomPage {
         const browser = await puppeteer.launch({
             executablePath: '/usr/bin/chromium-browser',
             headless: true,
+            args: ['--no-sandbox'],
         })
         const page = await browser.newPage();
         const customPage = new CustomPage(page);
