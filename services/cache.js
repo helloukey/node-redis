0502bfbccfcc10fa156ea39774512d69797aba38
Kunal <kunalukey32@gmail.com>
2024-01-29 13:01:11 +0530

    changes for CI configs

--- services/cache.js
+++ services/cache.js
@@ -1,8 +1,8 @@
 const util = require('util');
 const mongoose = require('mongoose');
 const redis = require('redis');
-const redisUrl = 'redis://127.0.0.1:6379';
-const client = redis.createClient(redisUrl);
+const keys = require('../config/keys');
+const client = redis.createClient(keys.redisUrl);
 client.hget = util.promisify(client.hget);
 
 const exec = mongoose.Query.prototype.exec;
